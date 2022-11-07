27a13cad3145e66bdb6e2ffeadb59fadd58327e8
aCute5 <costantini95dario@gmail.com>
2022-11-07 16:51:40 +0100

    script and style added

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+const cards = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        imgUser: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        imgUser: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        imgUser: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        imgUser: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        imgUser: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        imgUser: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+const container = document.getElementById("containercards");
+
+for(let i=0; i<cards.length; i++){
+    container.innerHTML += `<div class="card" style="width: 25rem;"> <img class="img-top" src="img/${cards[i].imgUser}"> <div class="card-info"> <h4>${cards[i].name}</h4> <span>${cards[i].job}</span> </div> </div>`
+}
